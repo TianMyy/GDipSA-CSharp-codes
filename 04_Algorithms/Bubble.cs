a97e57d44009815667f3b121dba04eba49d27c15
TianMyy <mingyu_tian@outlook.com>
2020-11-22 11:59:32 +0800

    Add files via upload

---
+++ 04_Algorithms/Bubble.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_DataStructure._05_Sort
+{
+    class Bubble
+    {
+        static void Main()
+        {
+            int[] number = new int[6] { 4, 3, 5, 6, 2, 1 };
+            for (int i = 0; i < number.Length - 1; i++)
+            {
+                for (int j = 0; j < number.Length - 1 - i; j++)
+                {
+                    if (number[j] > number[j + 1])
+                    {
+                        int temp = number[j];
+                        number[j] = number[j + 1];
+                        number[j + 1] = temp;
+                    }
+                }
+                for (int a = 0; a < number.Length; a++)
+                    Console.Write("{0}\t", number[a]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 04_Algorithms/StraightInsertion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_DataStructure._05_Sort
+{
+    class StraightInsertion
+    {
+        static void Main()
+        {
+            int[] data = new int[] { 42, 20, 17, 27, 13, 8, 17, 48 };
+            InsertSort(data);
+            foreach(var temp in data)
+            {
+                Console.Write(temp + "\t");
+            }
+        }
+
+        static void InsertSort(int[] dataArray)
+        {
+            for(int i = 1; i < dataArray.Length; i++)
+            {
+                int iValue = dataArray[i];
+                bool isInsert = false;
+                //compare element with index i with former elements, if greater then remain, if less then move
+                for (int j = i-1; j >= 0; j--)
+                {
+                    if(dataArray[j] > iValue)
+                    {
+                        dataArray[j + 1] = dataArray[j];
+                    }
+                    else
+                    {
+                        dataArray[j + 1] = iValue;
+                        isInsert = true;
+                        break;
+                    }
+                }
+                if(isInsert == false)
+                {
+                    dataArray[0] = iValue;
+                }
+            }
+        }
+    }
+}
