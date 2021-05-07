6df30fd08552f47fd04f4639b535d6b721fc2cee
TianMyy <mingyu_tian@outlook.com>
2021-05-07 22:59:59 +0800

    exercise for Interface class and method override concept

---
+++ 03object_oriented_programming/05-Interface/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOopCode_2._05_Interface
+{
+    class Contact:ISync
+    {
+        string name;
+        string phone;
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public Contact(string name,string phone)
+        {
+            Name = name;
+            Phone = phone;
+        }
+        public string Sync()
+        {
+            return string.Format("CO|{0}|{1}<end>", Name, Phone);
+        }
+    }
+}
