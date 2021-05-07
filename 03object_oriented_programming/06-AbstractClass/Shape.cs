f511adbb6adf239c4133abef1c90689b58faa227
TianMyy <mingyu_tian@outlook.com>
2021-05-07 23:00:45 +0800

    exercise for Abstract class and method override concept

---
+++ 03object_oriented_programming/06-AbstractClass/Shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOopCode_2._06_AbstractClass
+{
+    abstract class Shape
+    {
+        abstract public double Area();
+    }
+}
